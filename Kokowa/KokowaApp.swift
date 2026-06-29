0b10ba9511ed0fa1ae5bf224112c56fdf636b6eb
keikeikei0001 <keidm0802@gmail.com>
2026-06-29 20:24:31 +0900

    Initial Commit

---
+++ Kokowa/KokowaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KokowaApp.swift
+//  Kokowa
+//
+//  Created by けいけい on 2026/06/29.
+//
+
+import SwiftUI
+
+@main
+struct KokowaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
