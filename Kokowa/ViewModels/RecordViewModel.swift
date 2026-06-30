55a78ad5c3e47fcb2d111d329b8e3341ddc4f23d
keikeikei0001 <keidm0802@gmail.com>
2026-06-30 22:09:05 +0900

    無駄なものを削除。

---
+++ Kokowa/ViewModels/RecordViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  RecordViewModel.swift
+//  Kokowa
+//
+//  Created by けいけい on 2026/06/30.
+//
+
+import SwiftUI
+
+struct RecordViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecordViewModel()
+}
---
+++ Kokowa/Views/RecordView.swift
@@ -0,0 +1,18 @@
+//
+//  RecordView.swift
+//  Kokowa
+//
+//  Created by けいけい on 2026/06/30.
+//
+
+import SwiftUI
+
+struct RecordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecordView()
+}
