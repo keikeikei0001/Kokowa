15438de8c26a88860c7872a7f18f10f5861b2752
keikeikei0001 <keidm0802@gmail.com>
2026-07-03 15:11:18 +0900

    レベルアップ時の音声を追加！

---
+++ Kokowa/Models/SoundPlayer.swift
@@ -0,0 +1,48 @@
+//
+//  SoundPlayer.swift
+//  LifeIsRpg
+//
+//  Created by けいけい on 2024/12/22.
+//
+
+import SwiftUI
+import AVFoundation
+
+class SoundPlayer {
+    private var audioPlayer: AVAudioPlayer?
+    
+    init() {
+        try? AVAudioSession.sharedInstance().setCategory(.ambient, mode: .default, options: [.mixWithOthers])
+        try? AVAudioSession.sharedInstance().setActive(true)
+    }
+    
+    /// タスク完了時の効果音処理
+    func playTaskCompleteSound() {
+        if let asset = NSDataAsset(name: "男衆「イエーイ！」") {
+            do {
+                audioPlayer = try AVAudioPlayer(data: asset.data)
+                audioPlayer?.prepareToPlay()
+                audioPlayer?.play()
+            } catch {
+                print("音声の再生に失敗しました: \(error.localizedDescription)")
+            }
+        } else {
+            print("音声ファイルが見つかりません")
+        }
+    }
+    
+    /// タスク完了時の効果音処理
+    func playLevelUpSound() {
+        if let asset = NSDataAsset(name: "男衆「イエーイ！」") {
+            do {
+                audioPlayer = try AVAudioPlayer(data: asset.data)
+                audioPlayer?.prepareToPlay()
+                audioPlayer?.play()
+            } catch {
+                print("音声の再生に失敗しました: \(error.localizedDescription)")
+            }
+        } else {
+            print("音声ファイルが見つかりません")
+        }
+    }
+}
